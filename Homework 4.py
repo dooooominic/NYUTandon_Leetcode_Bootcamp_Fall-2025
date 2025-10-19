7f9d3b38e4f44e666c51dcfcccb5e4b96865d7da
Dominic Zhang <dooooominic@gmail.com>
2025-10-19 16:41:18 -0400

    Homework 4 Created

---
+++ Homework 4.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import defaultdict
+
+'''
+Homework Assignment 4: Leetcode Bootcamp Section 1 Fall 2025
+'''
+
+#Problem 1: 
+
+
+
+#Problem 2:
+
+
+
+#Problem 3:
