d011dd964421b198466385d549e4950e70a3b9e0
Dominic Zhang <dooooominic@gmail.com>
2025-11-06 17:15:27 -0500

    Added Homework 5 and Completed Problem 1

---
+++ Homework 5.py
@@ -0,0 +1,33 @@
+from typing import List
+
+'''
+Homework Assignment 5 (Week 6): Leetcode Bootcamp Section 1 Fall 2025
+'''
+
+#Problem 1: Leetcode 236: Lowest Common Ancestor of a Binary Tree
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+
+        return left or right  
+
+
+#Problem 2: 
+
+
+
+#Problem 3: 
